2aa0b1e2d7ece2f94ce13b8f73d2c291081cd84e
Josh Street <josh_s@ameritech.net>
2017-04-19 23:06:16 -0400

    initial commit

---
+++ src/main/java/com/dumbapp/DumbApplication.java
@@ -0,0 +1,12 @@
+package com.dumbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumbApplication.class, args);
+	}
+}
